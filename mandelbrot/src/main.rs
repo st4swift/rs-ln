f1621284f2ac08ff7a714e3bf450f74c225fd4b1
st4swift <st4swift@gmail.com>
2025-02-05 17:08:39 +0800

    mandelbrot

---
+++ mandelbrot/src/main.rs
@@ -0,0 +1,13 @@
+use num::Complex;
+
+fn complex_square_add_loop(c: Complex<f64>) {
+	let mut z = Complex{ re: 0.0, im: 0.0};
+	loop {
+		z = z * z + c;
+	}
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
