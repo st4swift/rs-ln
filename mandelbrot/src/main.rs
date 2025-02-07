04084f3aae086b11f671568fdb63ded90f0bcc67
st4swift <st4swift@gmail.com>
2025-02-07 16:27:31 +0800

    250207

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -16,7 +16,11 @@ fn complex_square_add_loop(c: Complex<f64>) {
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
 	let mut z = Complex { re: 0.0, img: 0.0 };
 	for i in 0..limit {
+		if z.norm_sqr() > 4.0 { return Some(i); }
+		z = z * z + c;
+	}
 
+	None
 } 
 
 
