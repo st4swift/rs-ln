e1e97efea281914aa330e7ea0c4ae56c1b3845b3
st4swift <st4swift@gmail.com>
2025-02-13 16:41:38 +0800

    250213

--- crash/src/main.rs
+++ crash/src/main.rs
@@ -1,16 +1,6 @@
+
 fn main() {
-    println!("Hello, crash!");
-
-	let tup: (u8, char, f32) = (90, 'k', 10.0);
-	let tup1 = (8, 4);
-
-	println!("tupe: {:?}.", tup);
-
-	let mut arr: [i64; 10];
-	arr = [890; 10];
-
-	println!("array: {:?}", arr);
-
-	println!("length of array: {}", arr.len());
-
+	let x = 10;
+	x / 10.0;
 }
+
