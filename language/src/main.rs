91fc4d7f176fec61b8fc6a38c74e26202750c3d9
st4swift <st4swift@gmail.com>
2025-02-13 22:56:09 +0800

    250213pm

---
+++ language/src/main.rs
@@ -0,0 +1,14 @@
+const HOURS_A_DAY: u8 = 24;
+
+fn main() {
+    println!("Hello, world!");
+
+	let mut spaces = "abcde";
+
+	println!("{}, {}, {}.", HOURS_A_DAY, spaces, spaces);
+
+	//let	 spaces = spaces.len();
+	spaces = "bnnnmbn";
+
+	println!("{}, {}, {}.", HOURS_A_DAY, spaces, spaces);
+}
