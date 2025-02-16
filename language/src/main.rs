9c7d3fc821e3526242c6d354947e67ce807a246e
st4swift <st4swift@gmail.com>
2025-02-16 21:46:42 +0800

    250216pm

--- language/src/main.rs
+++ language/src/main.rs
@@ -1,7 +1,18 @@
-const HOURS_A_DAY: u8 = 24;
+use std::io;
+
+//const HOURS_A_DAY: u8 = 24;
 
 fn main() {
-    println!("Hello, world!");
+	let a = [1, 2, 3, 4, 5];
+	println!("please enter an array index:");
+	let mut index = String::new();
+	io::stdin().read_line(&mut index).expect("Failed to read line.");
+	let index: usize = index.trim().parse().expect("Index entered was not a number.");
+	let element = a[index];
+
+	println!("the value of the element at index {index} is: {element}.");
+
+/*   println!("Hello, world!");
 
 	let mut spaces = "abcde";
 
@@ -16,4 +27,7 @@ fn main() {
 
 	let months: [&str; 12] = ["January", "February", "March", "April", "May", "June", "July",
 "August", "September", "October", "November", "December"];
+*/
+
+
 }
