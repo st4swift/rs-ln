9f398fab56345952c39bbbe9513c6615d7fee51b
st4swift <st4swift@gmail.com>
2025-02-25 16:57:45 +0800

    250225

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,8 +6,24 @@ struct User {
 	sign_in_count: u64,
 }
 
+fn build_user(email: String, username: String)-> User{
+	User{
+		active: true,
+		email,
+		username,
+		sign_in_count: 1,
+	}
+}
 
 fn main() {
+	let mut user1 = build_user(String::from("mail@example.com"), String::from("someone"));
+
+	user1.sign_in_count = 2;
+
+	let user2 = User {
+		username: String::from("user2-name"),
+		..user1
+	};
 
     println!("Hello, world!");
 }
---
+++ structs/target/debug/deps/structs-1dca08ecad21608d.d
@@ -0,0 +1,5 @@
+/Users/st/playground/rs-ln/structs/target/debug/deps/libstructs-1dca08ecad21608d.rmeta: src/main.rs
+
+/Users/st/playground/rs-ln/structs/target/debug/deps/structs-1dca08ecad21608d.d: src/main.rs
+
+src/main.rs:
