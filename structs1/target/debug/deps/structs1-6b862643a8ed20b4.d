8b96bc450b72c4483d688163144de80348f9e401
st4swift <st4swift@gmail.com>
2025-02-26 22:53:15 +0800

    struct

---
+++ structs1/target/debug/deps/structs1-6b862643a8ed20b4.d
@@ -0,0 +1,5 @@
+/Users/st/playground/rs-ln/structs1/target/debug/deps/structs1-6b862643a8ed20b4: src/main.rs
+
+/Users/st/playground/rs-ln/structs1/target/debug/deps/structs1-6b862643a8ed20b4.d: src/main.rs
+
+src/main.rs:
